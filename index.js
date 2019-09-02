7327634456ae1a6e7e543928731776fb907f2f24
Nathan Drezner <nathan@drezner.com>
2019-09-02 14:14:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -32,47 +32,46 @@
     });
 
     // SLIDER
-    
+
     // update hour filter when the slider is dragged
     document.getElementById('slider').addEventListener('input', function(e) {
       var year = parseInt(e.target.value);
       // update the map
-      map.setFilter('conundrums', ['==', ['number', ['get', 'Year']], year]);
+      map.setFilter('places', ['==', ['number', ['get', 'Year']], year]);
 
       // update text in the UI
       document.getElementById('active-year').innerText = year;
     });
 
+    // CLICKABLE POINTS
 
-    // // CLICKABLE POINTS
-
-    // // When a click event occurs on a feature in the places layer, open a popup at the
-    // // location of the feature, with description HTML from its properties.
-    // map.on('click', 'places', function (e) {
-    // var coordinates = e.features[0].geometry.coordinates.slice();
-    // var description = e.features[0].properties.description;
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'places', function (e) {
+    var coordinates = e.features[0].geometry.coordinates.slice();
+    var description = e.features[0].properties.description;
      
-    // // Ensure that if the map is zoomed out such that multiple
-    // // copies of the feature are visible, the popup appears
-    // // over the copy being pointed to.
-    // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    // coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    // }
+    // Ensure that if the map is zoomed out such that multiple
+    // copies of the feature are visible, the popup appears
+    // over the copy being pointed to.
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+    }
      
-    // new mapboxgl.Popup()
-    // .setLngLat(coordinates)
-    // .setHTML(description)
-    // .addTo(map);
-    // });
+    new mapboxgl.Popup()
+    .setLngLat(coordinates)
+    .setHTML(description)
+    .addTo(map);
+    });
      
-    // // Change the cursor to a pointer when the mouse is over the places layer.
-    // map.on('mouseenter', 'places', function () {
-    // map.getCanvas().style.cursor = 'pointer';
-    // });
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', function () {
+    map.getCanvas().style.cursor = 'pointer';
+    });
      
-    // // Change it back to a pointer when it leaves.
-    // map.on('mouseleave', 'places', function () {
-    // map.getCanvas().style.cursor = '';
-    // });
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', function () {
+    map.getCanvas().style.cursor = '';
+    });
 
 });
