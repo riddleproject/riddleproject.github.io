a06afa9b873bd96cb79269a88321f19d4f67ec63
Nathan Drezner <nathan@drezner.com>
2019-09-02 12:39:49 -0400

    Adjust styles

--- index.js
+++ index.js
@@ -41,21 +41,4 @@
       document.getElementById('active-year').innerText = year;
     });
 
-  // Add checkbox and label elements for the layer.
-  var input = document.createElement('input');
-  input.type = 'checkbox';
-  input.id = layerID;
-  input.checked = true;
-  filterGroup.appendChild(input);
-   
-  var label = document.createElement('label');
-  label.setAttribute('for', layerID);
-  label.textContent = symbol;
-  filterGroup.appendChild(label);
-   
-  // When the checkbox changes, update the visibility of the layer.
-  input.addEventListener('change', function(e) {
-  map.setLayoutProperty(layerID, 'visibility',
-  e.target.checked ? 'visible' : 'none');
-  });
 });
