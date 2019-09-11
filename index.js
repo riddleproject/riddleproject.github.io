b4257b870ea19805c558ab6793d4060a463bc748
Nathan Drezner <nathan@drezner.com>
2019-09-10 21:12:07 -0400

    Fix error?

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
- var filterGroup = document.getElementById('filter-group');
- mapboxgl.accessToken = 'pk.eyJ1IjoibmRyZXpuIiwiYSI6ImNqeXg2eDlhZzA0MzczZ28xeDdzNnNqY3kifQ.lxS44L-xGMpt-Wcv0vpHng';
-  // Add map
-  var map = new mapboxgl.Map({
-    container: 'map', // container id specified in the HTML
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
-    center: [-100.546666667, 46.0730555556], // initial map center in [lon, lat]
-    zoom: 3
+var filterGroup = document.getElementById('filter-group');
+mapboxgl.accessToken = 'pk.eyJ1IjoibmRyZXpuIiwiYSI6ImNqeXg2eDlhZzA0MzczZ28xeDdzNnNqY3kifQ.lxS44L-xGMpt-Wcv0vpHng';
+// Add map
+
+var map = new mapboxgl.Map({
+  container: 'map', // container id specified in the HTML
+  style: 'mapbox://styles/mapbox/light-v10', // style URL
+  center: [-100.546666667, 46.0730555556], // initial map center in [lon, lat]
+  zoom: 3
+});
+
+map.on('load', function() {
+  map.addLayer({
+    id: 'places',
+    type: 'circle',
+    source: {
+      type: 'geojson',
+      data: 'data/map_data_all.geojson'
+    },
+    paint: {
+      'circle-color': [
+        'interpolate',
+        ['exponential', 1],
+        ['number', ['get', 'Type']],
+        0, '#747EB3',
+        1, '#FF794B',
+        2, '#BFCAFF',
+        3, '#A5CC85',
+      ],
+      'circle-opacity': 0.8
+    },
+    'filter': ['==', ['number',['get', 'Year']], 1892]
   });
 
-  map.on('load', function() {
-    map.addLayer({
-      id: 'places',
-      type: 'circle',
-      source: {
-        type: 'geojson',
-        data: 'data/map_data_all.geojson'
-      },
-      paint: {
-        'circle-color': [
-          'interpolate',
-          ['exponential', 1],
-          ['number', ['get', 'Type']],
-          0, '#747EB3',
-          1, '#FF794B',
-          2, '#BFCAFF',
-          3, '#A5CC85',
-        ],
-        'circle-opacity': 0.8
-      },
-      'filter': ['==', ['number',['get', 'Year']], 1892]
-    });
+  // SLIDER
 
-    // SLIDER
+  // update hour filter when the slider is dragged
+  document.getElementById('slider').addEventListener('input', function(e) {
+    var year = parseInt(e.target.value);
+    // update the map
+    map.setFilter('places', ['==', ['number', ['get', 'Year']], year]);
 
-    // update hour filter when the slider is dragged
-    document.getElementById('slider').addEventListener('input', function(e) {
-      var year = parseInt(e.target.value);
-      // update the map
-      map.setFilter('places', ['==', ['number', ['get', 'Year']], year]);
+    // update text in the UI
+    document.getElementById('active-year').innerText = year;
+  });
 
-      // update text in the UI
-      document.getElementById('active-year').innerText = year;
-    });
+  // CLICKABLE POINTS
 
-    // CLICKABLE POINTS
-
-    // When a click event occurs on a feature in the places layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'places', function (e) {
+  // When a click event occurs on a feature in the places layer, open a popup at the
+  // location of the feature, with description HTML from its properties.
+  map.on('click', 'places', function (e) {
     var coordinates = e.features[0].geometry.coordinates.slice();
     var description = e.features[0].properties.description;
      
@@ -55,41 +56,41 @@
     // copies of the feature are visible, the popup appears
     // over the copy being pointed to.
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
      
     new mapboxgl.Popup()
     .setLngLat(coordinates)
     .setHTML(description)
     .addTo(map);
-    });
-     
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'places', function () {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-     
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'places', function () {
-    map.getCanvas().style.cursor = '';
-    });
-
-    document.getElementById('filters').addEventListener('change', function(e) {
-      var day = e.target.value;
-      // update the map filter
-      if (day === 'all') {
-        filterDay = ['!=', ['string', ['get', 'Type']], 'placeholder'];
-      } else if (day === 'loc') {
-        filterDay = ['match', ['get', 'Type'], [0], true, false];
-      } else (day === 'supper') {
-        filterDay = ['match', ['get', 'Type'], [1], true, false];
-      } else (day === 'tea') {
-        filterDay = ['match', ['get', 'Type'], [2], true, false];
-      } else (day === 'social') {
-        filterDay = ['match', ['get', 'Type'], [3], true, false];
-      } else {
-        console.log('error');
-      }
-      map.setFilter('collisions', ['all', filterDay]);
-    });
   });
+   
+  // Change the cursor to a pointer when the mouse is over the places layer.
+  map.on('mouseenter', 'places', function () {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+   
+  // Change it back to a pointer when it leaves.
+  map.on('mouseleave', 'places', function () {
+    map.getCanvas().style.cursor = '';
+  });
+
+  // document.getElementById('filters').addEventListener('change', function(e) {
+  //   var day = e.target.value;
+  //   // update the map filter
+  //   if (day === 'all') {
+  //     filterDay = ['!=', ['string', ['get', 'Type']], 'placeholder'];
+  //   } else if (day === 'loc') {
+  //     filterDay = ['match', ['get', 'Type'], [0], true, false];
+  //   } else (day === 'supper') {
+  //     filterDay = ['match', ['get', 'Type'], [1], true, false];
+  //   } else (day === 'tea') {
+  //     filterDay = ['match', ['get', 'Type'], [2], true, false];
+  //   } else (day === 'social') {
+  //     filterDay = ['match', ['get', 'Type'], [3], true, false];
+  //   } else {
+  //     console.log('error');
+  //   }
+  //   map.setFilter('collisions', ['all', filterDay]);
+  // });
+});
