7c25c4c94fe9cb9adc5d872b4827d87cc3d5a0f0
Nathan Drezner <nathan@drezner.com>
2019-09-11 16:54:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ var map = new mapboxgl.Map({
   zoom: 3
 });
 
+// Initialize toggles
+document.getElementById("showall").checked = false;
+document.getElementByID('filters').target.value = 'all'
 
 // BUILD MAP
 map.on('load', function() {
   // Initialize filters
   var filterYear = ['==', ['number', ['get', 'Year']], 1892];
   var filterType = ['!=', ['number', ['get', 'Type']], -1];
-  
-  // Initialize toggles
-  // document.getElementById("showall").checked = false;
-  // document.getElementByID('filters').target.value = 'all'
 
   map.addSource("conundrums", {
     type: "geojson",
