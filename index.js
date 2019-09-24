4e8e3e56a2a02e0cdd1555ed4e195bfd8e937837
Nathan Drezner <nathan@drezner.com>
2019-09-24 11:52:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmRyZXpuIiwiYSI6ImNqeXg2eDlhZzA0MzczZ28xeDdzN
 var map = new mapboxgl.Map({
   container: 'map', // container id specified in the HTML
   style: 'mapbox://styles/mapbox/light-v10', // style URL
-  center: [-80, 35], // initial map center in [lon, lat]
-  zoom: 2.5
+  center: [-90, 40], // initial map center in [lon, lat]
+  zoom: 2.2
 });
 
 // BUILD MAP
