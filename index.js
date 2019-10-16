96e0a360f3b6ae1f00de9fdd5614183a32d7a45f
Nathan Drezner <nathan@drezner.com>
2019-10-16 10:24:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,8 +55,7 @@ map.on('load', function() {
     if(e && e.which){ //if which property of event object is supported (NN4)
       e = e;
       characterCode = e.which; //character code is contained in NN4's which property
-    };
-    else{
+    } else{
       e = event;
       characterCode = e.keyCode; //character code is contained in IE's keyCode property
     };
@@ -64,8 +63,7 @@ map.on('load', function() {
     if(characterCode == 13){ //if generated character code is equal to ascii 13 (if enter key)
       document.forms[0].submit(); //submit the form
       return false;
-    };
-    else{
+    } else{
       return true;
     };
   };
