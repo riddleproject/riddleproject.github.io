b2755cbbabe2284112be0125bf19d770dd1984f2
Nathan Drezner <nathan@drezner.com>
2019-10-16 16:41:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ map.on('load', function() {
       //   * Yellow, 30px circles when point count is between 100 and 750
       //   * Pink, 40px circles when point count is greater than or equal to 750
       "circle-color": ["step",["get", "point_count"],"#51bbd6",100,"#f1f075",750,"#f28cb1"],
-      "circle-radius": ["step",["get", "point_count"],20,100,30,750,40],
+      "circle-radius": ["step",["get", "point_count"],5,10,20,40,100],
     },
   });
 
