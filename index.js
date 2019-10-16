073f4fc4c9c5aabdedfe48f7ec3c247a3c86de16
Nathan Drezner <nathan@drezner.com>
2019-10-16 15:20:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,29 +41,72 @@ map.on('load', function() {
   map.addSource("conundrums", {
     type: "geojson",
     data: "data.geojson",
+    cluster: true,
+    clusterMaxZoom: 14, // Max zoom to cluster points on
+    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
   });
 
   map.addLayer({
-    id: 'places',
-    type: 'circle',
-    source: 'conundrums',
+    id: "clusters",
+    type: "circle",
+    source: "earthquakes",
+    filter: ["has", "point_count"],
     paint: {
-      'circle-color': [
-        'interpolate',
-        ['exponential', 1],
-        ['number', ['get', 'Type']],
-        0, '#747EB3',
-        1, '#FF794B',
-        2, '#BFCAFF',
-        3, '#A5CC85',
-        4, '#FFD4A1',
-        5, '#58CC70',
-        6, '#901499',
-        7, '#2D2240',
-      ],
-      'circle-opacity': 0.8
-    },
-    'filter': ['all', startYearFilter, endYearFilter, typeFilter]
+      // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+      // with three steps to implement three types of circles:
+      //   * Blue, 20px circles when point count is less than 100
+      //   * Yellow, 30px circles when point count is between 100 and 750
+      //   * Pink, 40px circles when point count is greater than or equal to 750
+      "circle-color": ["step",["get", "point_count"],"#51bbd6",100,"#f1f075",750,"#f28cb1"],
+      "circle-radius": ["step",["get", "point_count"],20,100,30,750,40]
+    }
+  });
+
+  map.addLayer({
+    id: "cluster-count",
+    type: "symbol",
+    source: "earthquakes",
+    filter: ["has", "point_count"],
+    layout: {
+      "text-field": "{point_count_abbreviated}",
+      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+      "text-size": 12
+    }
+  });
+
+  map.addLayer({
+    id: "unclustered-point",
+    type: "circle",
+    source: "earthquakes",
+    filter: ["!", ["has", "point_count"]],
+    paint: {
+      "circle-color": "#11b4da",
+      "circle-radius": 4,
+      "circle-stroke-width": 1,
+      "circle-stroke-color": "#fff"
+    }
+  });
+
+  // inspect a cluster on click
+  map.on('click', 'clusters', function (e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: ['clusters'] });
+    var clusterId = features[0].properties.cluster_id;
+    map.getSource('earthquakes').getClusterExpansionZoom(clusterId, function (err, zoom) {
+      if (err)
+        return;
+     
+      map.easeTo({
+        center: features[0].geometry.coordinates,
+        zoom: zoom
+      });
+    });
+  });
+   
+  map.on('mouseenter', 'clusters', function () {
+    map.getCanvas().style.cursor = 'pointer';
+  });
+  map.on('mouseleave', 'clusters', function () {
+    map.getCanvas().style.cursor = '';
   });
 
   var startyear = 1892
