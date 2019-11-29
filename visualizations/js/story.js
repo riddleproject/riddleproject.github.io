56b3aebaa23f6a61a1322ea93db28443f77ad252
Nathan Drezner <nathan@drezner.com>
2019-11-29 13:47:23 -0500

    Update story.js

--- visualizations/js/story.js
+++ visualizations/js/story.js
@@ -3,11 +3,10 @@ mapboxgl.accessToken =
 
 
 // Initialize filters
-var curTypes = [0,1,2,3,4,5,6,7,8,9,10]
 var toggles = ['banq', 'bchn', 'bna', 'ean', 'ebof', 'ink', 'lcsoc','lcsup', 'lctea', 'nys', 'ppp'];
 var startYearFilter = ['>=', ['number', ['get', 'Year']], 0];
 var endYearFilter = ['<=', ['number', ['get', 'Year']], 3000];
-var typeFilter = ['match', ['get', 'Type'], curTypes, true, false]
+var typeFilter = ['match', ['get', 'Type'], [0,1,2,3,4,5,6,7,8,9,10], true, false]
 var onlyMenus = ['!=', ['number', ['get', 'has_menu']], -1];
 
 var map = new mapboxgl.Map({
@@ -60,28 +59,28 @@ var chapters = {
     center: [-60.88620, 48.62161],
     zoom: 1,
     pitch: 0,
-    filter: [startYearFilter, endYearFilter, typeFilter, onlyMenus],
+    filter: [],
   },
   "british-riddles": {
     center: [-3.22679, 53.61753],
     bearing: -42.13,
     zoom: 4.77,
     pitch: 54.50,
-    filter: [startYearFilter, endYearFilter, typeFilter, onlyMenus],
+    filter: [],
   },
   "ebofs": {
     center: [-3.22679, 53.61753],
     bearing: -42.13,
     zoom: 4.77,
     pitch: 54.50,
-    filter: [startYearFilter, endYearFilter, ['match', ['get', 'Type'], [4], true, false], onlyMenus],
+    filter: ['match', ['get', 'Type'], [4], true, false],
   },
   "bc-riddles": {
     bearing: 36.27,
     center: [-123.06917, 53.29594],
     zoom: 3.98,
     pitch: 54.50,
-    filter: [startYearFilter, endYearFilter, typeFilter, onlyMenus],
+    filter: [],
   },
 };
 
