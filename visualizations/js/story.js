f7eff17fae3443f2efa5c3a0a069c9a4bb084755
Nathan Drezner <nathan@drezner.com>
2019-11-29 12:57:31 -0500

    fix linking

--- visualizations/js/story.js
+++ visualizations/js/story.js
@@ -74,35 +74,6 @@ var chapters = {
     speed: 0.6,
     pitch: 40
   },
-  woolwich: {
-    bearing: 90,
-    center: [0.05991101, 51.48752939],
-    zoom: 12.3
-  },
-  gloucester: {
-    bearing: 45,
-    center: [-0.18335806, 51.49439521],
-    zoom: 15.3,
-    pitch: 20,
-    speed: 0.5
-  },
-  "caulfield-gardens": {
-    bearing: 180,
-    center: [-0.19684993, 51.5033856],
-    zoom: 12.3
-  },
-  telegraph: {
-    bearing: 90,
-    center: [-0.10669358, 51.51433123],
-    zoom: 17.3,
-    pitch: 40
-  },
-  "charing-cross": {
-    bearing: 90,
-    center: [-0.12416858, 51.50779757],
-    zoom: 14.3,
-    pitch: 20
-  }
 };
 
 // On every scroll event, check which element is on screen
